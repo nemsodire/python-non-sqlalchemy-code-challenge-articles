3ce7537c7afb658285aa660e2446e7bd316762da
neema <neemabridgetteodire2@gmail.com>
2024-05-27 23:42:26 +0300

    code challenge 1

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,18 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if len(name) == 0:
+            raise ValueError("Name cannot be empty")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Name cannot be changed")
 
     def articles(self):
         pass
@@ -20,10 +26,43 @@ class Author:
     def topic_areas(self):
         pass
 
+
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if not (2 <= len(name) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters")
+        if not isinstance(category, str):
+            raise TypeError("Category must be a string")
+        if len(category) == 0:
+            raise ValueError("Category cannot be empty")
+        self._name = name
+        self._category = category
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if not (2 <= len(value) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Category must be a string")
+        if len(value) == 0:
+            raise ValueError("Category cannot be empty")
+        self._category = value
 
     def articles(self):
         pass
@@ -36,3 +75,43 @@ class Magazine:
 
     def contributing_authors(self):
         pass
+
+
+class Article:
+    def __init__(self, author, magazine, title):
+        if not isinstance(author, Author):
+            raise TypeError("author must be an instance of Author")
+        if not isinstance(magazine, Magazine):
+            raise TypeError("magazine must be an instance of Magazine")
+        if not isinstance(title, str):
+            raise TypeError("Title must be of type str")
+        if not (5 <= len(title) <= 50):
+            raise ValueError("Title must be between 5 and 50 characters, inclusive")
+
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise TypeError("author must be an instance of Author")
+        self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            raise TypeError("magazine must be an instance of Magazine")
+        self._magazine = value
